9a1f7592759ff1e683af3f5f519936c0486e919b
priyanshu639 <priyanshu63930@gmail.com>
2025-11-15 11:20:11 +0530

    conditional based problem

---
+++ conditionalStmt.py
@@ -0,0 +1,36 @@
+# compare to number
+
+# num1=int(input("enter number 1: "))
+# num2=(int(input("Enter number 2: ")))
+# if(num1>num2):
+#     print(f"num1 is greater {num1}")
+# elif num1==num2:
+#     print(f"num1 is equla to num2 {num1}")
+# else:
+#     print(f"num2 is greater {num2}")
+    
+    
+    
+    
+# Greet by Grender
+# num1=str(input("enter charater 1: "))
+
+# if num1=='m' or num1=='M':
+#     print("Hello sir")
+# elif num1=='F' or num1=='f':
+#     print("hello mam")
+# else:
+#     print("hello No male No female")
+    
+    
+    
+# leap year checker
+num1=int(input("enter year: "))
+if(num1%100!=0 and num1%4==0):
+    print(f"leap year {num1}")
+elif num1%100==0 and num1%400==0:
+    print(f"leap year {num1}")
+
+else:
+    print("not leap year")
+    
